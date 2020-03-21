e3a6907c566f2b1275ad0f2a64b503e4fb43884d
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-21 19:03:49 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,12 +1,10 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy
   attr_accessor :name, :breed, :age
-  
+
   def initialize(name, breed, age)
     @name = name
-    @age = age
     @breed = breed
+    @age = age
   end
-  
-  
 end
